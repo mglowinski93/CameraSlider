21ae889a15ba3bf2ada9761c0ca1a3c806e5bbd4
Mateusz Glowinski <mateusz.glowinski@digitalstrom.com>
2019-05-12 14:46:37 +0200

    Read and initate state - nano receiver

---
+++ Nano Receiver/sketch_jan09a/sketch_jan09a.ino
@@ -0,0 +1,66 @@
+/*
+Library include
+*/
+#include <HID.h> //biblioteka do łącza USB  https://www.arduino.cc/en/Reference/HID
+#include <SPI.h> //serial peripheral interface pozwala na komunikacje z innymi mikrokontrolerami na krotkich dystansach     https://www.arduino.cc/en/reference/SPI
+#include <nRF24L01.h> //Library can be downloaded from here: https://github.com/nRF24/RF24
+#include <RF24.h> // biblioteka do modulu NRF
+ 
+/*
+Define global variables
+*/
+const int SW_pin = 2; // digital pin connected to switch output. In our case it will be used to interrupt move of stepper (IN4)
+const int X_pin = 0; // analog pin connected to X output
+const int Y_pin = 1; // analog pin connected to Y output
+const int CE = 7; //need for NRF24L01
+const int CSN = 8; //need for NRF24L01
+bool SW; //variable reserved for JoyStick Push State. WILL WE NEED THIS FOR SOMETHING?
+int Xasis=0, Yasis=0; //JoyStick position
+ 
+/*
+Prepare NRF Object
+More about NRF24L01: https://howtomechatronics.com/tutorials/arduino/arduino-wireless-communication-nrf24l01-tutorial/
+*/
+RF24 radio(CE, CSN); // CE, CSN you can define as you wish:)
+const byte address[6] = "00001"; //address (pipe) through which two moduled will communicate. There is possibility to choose 5  letter string. This enables the choose to which receiver we will talk. It has to be the same on transmitter and receiver.  
+int joystick_position[3] = {0,0,0}; //array with data to send via NRF24L01
+ 
+/*
+Read Joystick State
+*/
+void joyStick()
+{
+  SW = digitalRead(SW_pin);
+  Xasis = analogRead(X_pin);
+  Yasis = analogRead(Y_pin);
+}
+ 
+void setup() {
+  //There is no need to initializate analog ports. By default there are inputs. That is why i'm not initializating
+  Serial.begin(9600);
+  Serial.println("Preparing setup");
+  pinMode(SW_pin, INPUT);
+  digitalWrite(SW_pin, HIGH);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);//This function set the Power Amplifier Level, depends on distance between modules. It distance will be more than 1m, value needs to be changed
+  radio.stopListening(); //This function set module as Transmitter
+  Serial.println("Setup done");
+}
+ 
+void loop() {
+  joyStick();
+  joystick_position[0] = Xasis;
+  joystick_position[1] = Yasis;
+  joystick_position[2] = SW;
+  Serial.print("X-value: ");
+  Serial.println(joystick_position[0]);
+  Serial.print("Y-value: ");
+  Serial.println(joystick_position[1]);
+  Serial.print("SW-state: ");
+  Serial.println(joystick_position[2]);
+  Serial.println("===============");
+ 
+  //I'm not sure if it will work. I've never used that in such a way.
+  radio.write(&joystick_position, sizeof(joystick_position));
+}
--- Uno/sketch_jan09b/sketch_jan09b.ino
+++ Uno/sketch_jan09b/sketch_jan09b.ino
@@ -1,188 +1,113 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#include <CustomStepper.h>
- 
-const unsigned short int MAIN_DELAY = 1000;
-const unsigned short int requiredStep = 2048;
-const int ENABLE_EASY_DRIVER = 0;
-const int STEP_PIN_EASY_DRIVER = 4;
-const int DIR_PIN_EASY_DRIVER = 5;
 const int CE = 6;
 const int CSN = 7;
 int joystickData[3] = {0};
-int leftLimitSwitch = 2;
-int rightLimitSwitch = 3;
-int leftEndSwitchState = 0;
-int rightEndSwitchState = 0;
-const int speedSetpoint1 = 1;
-const int speedSetpoint2 = 2;
-unsigned int speedValue = 1;
-int rotateStepperSetpointSlow = 12; // RPM can be set from 1RPM to 15RPM
-int rotateStepperSetpointFast = 20; // RPM can be set from 1RPM to 15RPM
 bool joystickButtonPressed = 0;
-CustomStepper stepper(8, 9, 10, 11, (byte[]){8, B1000, B1100, B0100, B0110, B0010, B0011, B0001, B1001}, 4075.7728395, 12, STOP); //more: https://playground.arduino.cc/Main/CustomStepper
- 
-RF24 radio(CE, CSN); // CE, CSN
 const byte address[6] = "00001";
- 
-void set_speed()
-{
-  if(speedValue == speedSetpoint1)
-  {
-    stepper.setRPM(rotateStepperSetpointSlow);
-    speedValue = speedSetpoint2;
-  }
-  else
-  {
-    stepper.setRPM(rotateStepperSetpointFast);
-    speedValue = speedSetpoint1;
-  }
-}
+const int smallSteperCW = 8; //28BYJ-48 ClockWise
+const int smallSteperCCW = 9; //28BYJ-48 CounterClockWise
+const int easyDriverCW = 10; //EasyDriver CounterClockWise
+const int easyDriverCCW = 11; //EasyDriver CounterClockWise
+const int speedChange = 12; //Choose Speed Value
 
-void moveEasyDriverRight()
-{        
-  digitalWrite(ENABLE_EASY_DRIVER, LOW);
-  digitalWrite(DIR_PIN_EASY_DRIVER, LOW);
-  digitalWrite(STEP_PIN_EASY_DRIVER, HIGH);
-}
-
-void moveEasyDriverLeft()
-{
-  digitalWrite(ENABLE_EASY_DRIVER, LOW);
-  digitalWrite(DIR_PIN_EASY_DRIVER, HIGH);
-  digitalWrite(STEP_PIN_EASY_DRIVER, HIGH);
-}
-
-void stopEasyDriver()
-{
-   digitalWrite(ENABLE_EASY_DRIVER, HIGH);
-   digitalWrite(DIR_PIN_EASY_DRIVER, LOW);
-   digitalWrite(STEP_PIN_EASY_DRIVER, LOW);  
-}
+RF24 radio(CE, CSN); // CE, CSN
 
 void setup()
 {
-  Serial.println("Setting up device");
-  pinMode(ENABLE_EASY_DRIVER, OUTPUT);
-  digitalWrite(ENABLE_EASY_DRIVER, HIGH);
-  pinMode(STEP_PIN_EASY_DRIVER, OUTPUT);
-  pinMode(DIR_PIN_EASY_DRIVER, OUTPUT);
-  digitalWrite(ENABLE_EASY_DRIVER, HIGH);
-  pinMode(leftLimitSwitch, INPUT);
-  digitalWrite(leftLimitSwitch, HIGH);
-  pinMode(rightLimitSwitch, INPUT);
-  digitalWrite(rightLimitSwitch, HIGH);
-  attachInterrupt(digitalPinToInterrupt(leftLimitSwitch), moveEasyDriverRight, LOW);
-  attachInterrupt(digitalPinToInterrupt(rightLimitSwitch), moveEasyDriverLeft, LOW);
   Serial.begin(9600);
-  stepper.setSPR(4075.7728395);
+  Serial.println("Setting up radio module");
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
   Serial.println("Setup done");
 }
+
 void loop()
 {
-  leftEndSwitchState = digitalRead(leftLimitSwitch);
-  rightEndSwitchState = digitalRead(rightLimitSwitch);
-  Serial.println("==============");
-  Serial.print("Left end state switch state: ");
-  Serial.println(leftEndSwitchState);
-  Serial.print("Right end state switch state: ");
-  Serial.println(rightEndSwitchState);
- 
+  Serial.println("=============="); 
   /*
     Start reading from NRF module
   */
-  if ( radio.available())
+  if ( radio.available() )
   {
       radio.read(&joystickData, sizeof(joystickData)); //read data
       Serial.println(joystickData[0]); // X-Position
       Serial.println(joystickData[1]); // Y-position
       Serial.println(joystickData[2]); // SW Button 
+      
       /*
         Set speed dependently on SW button state
       */
       if(joystickData[2] == 1 && joystickButtonPressed == 0)
       {
-        set_speed();
+        digitalWrite(speedChange, HIGH);
         joystickButtonPressed = 1;
       }
       if(joystickData[2] == 0)
       {
+        digitalWrite(speedChange, LOW);
         joystickButtonPressed = 0;  
       }
+
  
       /*
         Move 28BYJ-48
       */
-      
       if(joystickData[1] > 800)
       {
         Serial.println("Rotating 28BYJ-48 ClockWise");
-        if (stepper.isDone())
-        {
-          stepper.setDirection(CW);
-          stepper.rotate();
-        }
+        digitalWrite(smallSteperCW, HIGH);
+        digitalWrite(smallSteperCCW, LOW);
       } else if(joystickData[1] < 300)
       {
         Serial.println("Rotating 28BYJ-48 CounterClockWise");
-        if (stepper.isDone())
-        {
-          stepper.setDirection(CCW);
-          stepper.rotate();
-        }
+        digitalWrite(smallSteperCW, LOW);
+        digitalWrite(smallSteperCCW, HIGH);
       }else
       {
         Serial.print("No 28BYJ-48 action. Value of Y-position is:");
         Serial.println(joystickData[1]);
-        if (stepper.isDone())
-        {
-          stepper.setDirection(STOP);
-          stepper.rotate();
-        }
+        digitalWrite(smallSteperCW, LOW);
+        digitalWrite(smallSteperCCW, LOW);
       }
+
        /*
-        Easy Driver
-        ENABLE_EASY_DRIVER = 2;
-        STEP_PIN_EASY_DRIVER = 3;
-        DIR_PIN_EASY_DRIVER = 4;
+        Move EasyDriver
       */
-      
-      if(joystickData[2] > 800)
+      if(joystickData[0] > 800)
       {
-        Serial.println("Rotating Easy Driver ClockWise");
-        moveEasyDriverLeft();
-        
-      } else if(joystickData[2] < 300)
+        Serial.println("Rotating EasyDriver ClockWise");
+        digitalWrite(easyDriverCW, HIGH);
+        digitalWrite(easyDriverCCW, LOW);
+      } else if(joystickData[0] < 300)
       {
         Serial.println("Rotating EasyDriver CounterClockWise");
-        moveEasyDriverRight();
+        digitalWrite(easyDriverCW, LOW);
+        digitalWrite(easyDriverCCW, HIGH);
       }else
       {
         Serial.print("No EasyDriver action. Value of Y-position is:");
-        Serial.println(joystickData[2]);
-        stopEasyDriver();
+        Serial.println(joystickData[0]);
+        digitalWrite(easyDriverCW, LOW);
+        digitalWrite(easyDriverCCW, LOW);
       }
-      
+  
   } else 
   {
     Serial.println("No data received from transmitter");
     Serial.println("Stopping Easy Driver");
-    stopEasyDriver();
+    digitalWrite(easyDriverCW, LOW);
+    digitalWrite(easyDriverCCW, LOW);
     Serial.println("Easy Driver Stopped");
     Serial.println("Stopping 28BYJ-48");
-    stepper.setDirection(STOP);
-    stepper.rotate();  
+    digitalWrite(smallSteperCW, LOW);
+    digitalWrite(smallSteperCCW, LOW);
     Serial.println("28BYJ-48  stopped");
   }
   Serial.println("==============");
   Serial.println();
   Serial.println();
-  stepper.run();
-  if(MAIN_DELAY !=0)
-    {delay(MAIN_DELAY);}
 }
